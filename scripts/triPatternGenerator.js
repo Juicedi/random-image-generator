967f7072ae9c68030449303c9a8d03aa916b5480
Jussi Latvaniemi <jussi.latvaniemi@gmail.com>
2022-07-24 16:45:40 +0300

    Add helper classes

--- scripts/triPatternGenerator.js
+++ scripts/triPatternGenerator.js
@@ -9,42 +9,91 @@ function triPatternGenerator(canvas) {
   canvas.width = canvasDimentions[x]
   canvas.height = canvasDimentions[y]
   const ctx = canvas.getContext('2d', { alpha: false });
-  const getRandomInt = (max) => Math.floor(Math.random() * max);
-  const generateRandomColor = () => `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
-  const numberOfRows = 40;
+  const numberOfRows = 100;
   const triangleMaxSize = 80;
 
   // Move triangles of the canvas edge
   const offset = 10;
 
-  const createTriangle = () => {
-    const tri = [
-      [getRandomInt(triangleMaxSize), getRandomInt(triangleMaxSize)],
-      [getRandomInt(triangleMaxSize), getRandomInt(triangleMaxSize)],
-      [0, 0]
-    ];
+  class Vector {
+    constructor(coordinate1, coordinate2) {
+      this.x = coordinate2[x] - coordinate1[x];
+      this.y = coordinate2[y] - coordinate1[y];
+    }
 
-    const [c1, c2] = tri;
-    const vector = [c2[x] - c1[x], c2[y] - c1[y]];
-    const middle = [(c1[x] + c2[x]) / 2, (c1[y] + c2[y]) / 2];
-    const perpendicularVector = [vector[y], -vector[x]]
-    const perpendicularVectorEndPoint = [perpendicularVector[x] + middle[x], perpendicularVector[y] + middle[y]];
-    tri[2] = perpendicularVectorEndPoint;
-    return tri;
+    getPerpendicularVector () {
+      return new Vector([0, 0], [this.y, -this.x]);
+    }
+
+    getEndCoordinate (startCoordinate) {
+      return [this.x + startCoordinate[x], this.y + startCoordinate[y]];
+    }
+  }
+
+  class Line {
+    constructor(coordinate1, coordinate2) {
+      this.x1 = coordinate1[x];
+      this.x2 = coordinate2[x];
+      this.y1 = coordinate1[y];
+      this.y2 = coordinate2[y];
+    }
+
+    getMiddlePoint () {
+      return [
+        (this.x1 + this.x2) / 2,
+        (this.y1 + this.y2) / 2
+      ];
+    }
+  }
+
+  class RGB {
+    constructor(r, g, b) {
+      this.r = r;
+      this.g = g;
+      this.b = b;
+    }
+
+    toString() {
+      return `rgb(${this.r}, ${this.g}, ${this.b})`;
+    }
+  }
+
+  const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+  const getColor = (columnNumber, rowNumber) => {
+    const rightValue = 255 * (columnNumber / numberOfRows);
+    const bottomValue = 255 * (rowNumber / numberOfRows);
+    const leftValue = 255 - (255 * (columnNumber / numberOfRows));
+    const topValue = 255 - (255 * (rowNumber / numberOfRows));
+    return (new RGB(getRandomInt(topValue), leftValue, bottomValue)).toString();
   };
 
-  const triangleCenter = (triangle) => [
-    (triangle[0][x] + triangle[1][x] + triangle[2][x]) / 3,
-    (triangle[0][y] + triangle[1][y] + triangle[2][y]) / 3
-  ];
+  class Triangle {
+    constructor() {
+      this.corners = [
+        [getRandomInt(triangleMaxSize), getRandomInt(triangleMaxSize)],
+        [getRandomInt(triangleMaxSize), getRandomInt(triangleMaxSize)],
+        [0, 0]
+      ];
 
-  const moveTriangle = (coordinates, triangle) => {
-    const center = triangleCenter(triangle);
-    const delta = [coordinates[x] - center[x], coordinates[y] - center[y]];
-    triangle[0] = [triangle[0][x] + delta[x], triangle[0][y] + delta[y]];
-    triangle[1] = [triangle[1][x] + delta[x], triangle[1][y] + delta[y]];
-    triangle[2] = [triangle[2][x] + delta[x], triangle[2][y] + delta[y]];
-    return triangle;
+      const line = new Line(this.corners[0], this.corners[1]);
+      const vector = new Vector(this.corners[0], this.corners[1]);
+      const perpendicularVector = vector.getPerpendicularVector();
+      this.corners[2] = perpendicularVector.getEndCoordinate(line.getMiddlePoint());
+    }
+
+    move (coordinates) {
+      const center = this.getCenter();
+      const delta = [coordinates[x] - center[x], coordinates[y] - center[y]];
+      this.corners = this.corners.map((coordinate) => [coordinate[x] + delta[x], coordinate[y] + delta[y]]);
+    };
+
+    getCenter () {
+      return [
+        (this.corners[0][x] + this.corners[1][x] + this.corners[2][x]) / 3,
+        (this.corners[0][y] + this.corners[1][y] + this.corners[2][y]) / 3
+      ];
+    }
   };
 
   // This is assuming that the canvas is square
@@ -57,33 +106,18 @@ function triPatternGenerator(canvas) {
   ctx.fillStyle = '#fff';
   ctx.fillRect(0, 0, canvasDimentions[x], canvasDimentions[y]);
 
-  const getColor = (columnNumber, rowNumber) => {
-    // return generateRandomColor();
-    const rightValue = 255 * (columnNumber / numberOfRows);
-    const bottomValue = 255 * (rowNumber / numberOfRows);
-    const leftValue = 255 - (255 * (columnNumber / numberOfRows));
-    const topValue = 255 - (255 * (rowNumber / numberOfRows));
-
-    const r = getRandomInt(topValue);
-    const g = leftValue;
-    const b = bottomValue;
-
-    return `rgb(${r}, ${g}, ${b})`;
-  };
-
   for (let i = 0; i < numberOfTris; i++) {
-    const triangle = createTriangle();
+    const triangle = new Triangle();
     const columnNumber = (Math.floor(i / numberOfRows));
     const rowNumber = (i % numberOfRows);
     const xCoord = (xSpacing * columnNumber) + offset;
     const yCoord = (ySpacing * rowNumber) + offset;
 
-    moveTriangle([xCoord, yCoord], triangle);
-
+    triangle.move([xCoord, yCoord]);
     ctx.beginPath();
-    ctx.moveTo(...triangle[0]);
-    ctx.lineTo(...triangle[1]);
-    ctx.lineTo(...triangle[2]);
+    ctx.moveTo(...triangle.corners[0]);
+    ctx.lineTo(...triangle.corners[1]);
+    ctx.lineTo(...triangle.corners[2]);
     ctx.fillStyle = getColor(columnNumber, rowNumber);
     ctx.fill();
   }
